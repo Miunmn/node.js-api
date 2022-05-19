e06a21e199196a037e8b46658b6557b8c51395db
miunmnn <esteban.principe@utec.edu.pe>
2022-05-19 12:44:35 -0500

    Update statud code in case of failure

--- dev/pages/api/getBranchCommits.js
+++ dev/pages/api/getBranchCommits.js
@@ -7,6 +7,6 @@ export default async function getBranchCommits(req, res) {
   }
   catch(err){
     console.log('err', err);
-    res.status(err.status).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
--- dev/pages/api/getUserInfo.js
+++ dev/pages/api/getUserInfo.js
@@ -14,6 +14,6 @@ export default async function getUserInfo(req, res) {
   }
   catch(err){
     console.log('err', err);
-    res.status(err.status).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
